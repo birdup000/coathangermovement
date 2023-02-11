ae6bbebdb45458f2c2cfb7e76eab909ca4941b99
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-02-11 17:56:06 -0600

    Update workout.py

--- workout.py
+++ workout.py
@@ -1,81 +1,51 @@
 import tkinter as tk
+import os
 from tkinter import ttk
 from PIL import Image, ImageTk
-import os
-
 root = tk.Tk()
 root.title("Workout Log")
-
-def add_workout():
-    workout_type = workout_var.get()
-    if workout_type == "Weightlifting":
-        weightlifting_type = weightlifting_var.get()
-        workout_list.insert(tk.END, f"{workout_type}: {weightlifting_type}")
-    elif workout_type == "Swimming":
-        swimming_type = swimming_var.get()
-        workout_list.insert(tk.END, f"{workout_type}: {swimming_type}")
-    elif workout_type == "Yoga":
-        yoga_type = yoga_var.get()
-        workout_list.insert(tk.END, f"{workout_type}: {yoga_type}")
-    else:
-        workout_list.insert(tk.END, workout_var.get())
-    save_workouts()
-
-def remove_workout():
-    workout_list.delete(tk.ANCHOR)
-    save_workouts()
-
-def save_workouts():
-    with open("workouts.txt", "w") as f:
-        for item in workout_list.get(0, tk.END):
-            f.write(f"{item}\n")
-
-def load_workouts():
-    if os.path.exists("workouts.txt"):
-        with open("workouts.txt") as f:
-            for item in f:
-                workout_list.insert(tk.END, item.strip())
-
-# Create a dictionary of workout icons
+# Workout list
+workout_list = tk.Listbox(root)
+workout_list.pack(pady=10)
+# Workout icons
 icon_path = "workout_icons"
-workout_icons = {
-    "Running": ImageTk.PhotoImage(Image.open(os.path.join(icon_path, "running.png"))),
-    "Cycling": ImageTk.PhotoImage(Image.open(os.path.join(icon_path, "cycling.png"))),
-    "Weightlifting": ImageTk.PhotoImage(Image.open(os.path.join(icon_path, "weightlifting.png"))),
-    "Swimming": ImageTk.PhotoImage(Image.open(os.path.join(icon_path, "swimming.png"))),
-    "Yoga": ImageTk.PhotoImage(Image.open(os.path.join(icon_path, "yoga.png")))
-}
-
-# Create a drop-down menu for selecting the workout type
+try:
+    workout_icons = {
+        "Running": ImageTk.PhotoImage(Image.open(os.path.join(icon_path, "running.png"))),
+        "Cycling": ImageTk.PhotoImage(Image.open(os.path.join(icon_path, "cycling.png"))),
+        "Weightlifting": ImageTk.PhotoImage(Image.open(os.path.join(icon_path, "weightlifting.png"))),
+        "Swimming": ImageTk.PhotoImage(Image.open(os.path.join(icon_path, "swimming.png"))),
+        "Yoga": ImageTk.PhotoImage(Image.open(os.path.join(icon_path, "yoga.png")))
+    }
+except FileNotFoundError as e:
+    print(f"The file could not be found: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+# Workout dropdown
 workout_var = tk.StringVar(value="Running")
 workout_dropdown = tk.OptionMenu(root, workout_var, *workout_icons.keys(), image=workout_icons["Running"], compound="left")
 workout_dropdown.pack(pady=10)
-
-# Create a drop-down menu for selecting the weightlifting type
+# Dropdown menu values
+workout_types = {
+    "Weightlifting": ["Bench Press", "Deadlift", "Squat", "Bicep Curls", "Tricep Dips"],
+    "Swimming": ["Freestyle", "Backstroke", "Breaststroke", "Butterfly"],
+    "Yoga": ["Vinyasa", "Hatha", "Bikram", "Ashtanga", "Iyengar"]
+}
+# Subdropdown frames
 weightlifting_frame = tk.Frame(root)
-weightlifting_var = tk.StringVar(value="Bench Press")
-weightlifting_types = [    "Bench Press",    "Deadlift",    "Squat",    "Bicep Curls",    "Tricep Dips"]
-weightlifting_dropdown = ttk.Combobox(weightlifting_frame, textvariable=weightlifting_var, values=weightlifting_types)
-weightlifting_dropdown.pack(pady=10)
-weightlifting_frame.pack()
-
-# Create a drop-down menu for selecting the swimming type
 swimming_frame = tk.Frame(root)
-swimming_var = tk.StringVar(value="Freestyle")
-swimming_types = [    "Freestyle",    "Backstroke",    "Breaststroke",    "Butterfly"]
-swimming_dropdown = ttk.Combobox(swimming_frame, textvariable=swimming_var, values=swimming_types)
-swimming_dropdown.pack(pady=10)
-swimming_frame.pack()
-
-# Create a drop-down menu for selecting the yoga type
 yoga_frame = tk.Frame(root)
+# Subdropdown vars and comboboxes
+weightlifting_var = tk.StringVar(value="Bench Press")
+weightlifting_dropdown = ttk.Combobox(weightlifting_frame, textvariable=weightlifting_var, values=workout_types["Weightlifting"])
+weightlifting_dropdown.pack(pady=10)
+swimming_var = tk.StringVar(value="Freestyle")
+swimming_dropdown = ttk.Combobox(swimming_frame, textvariable=swimming_var, values=workout_types["Swimming"])
+swimming_dropdown.pack(pady=10)
 yoga_var = tk.StringVar(value="Vinyasa")
-yoga_types = [    "Vinyasa",    "Hatha",    "Bikram",    "Ashtanga",    "Iyengar"]
-yoga_dropdown = ttk.Combobox(yoga_frame, textvariable=yoga_var, values=yoga_types)
+yoga_dropdown = ttk.Combobox(yoga_frame, textvariable=yoga_var, values=workout_types["Yoga"])
 yoga_dropdown.pack(pady=10)
-yoga_frame.pack()
-
-# Show only the relevant drop-down menu
+# Show relevant subdropdown
 def show_dropdown(workout_type):
     weightlifting_frame.pack_forget()
     swimming_frame.pack_forget()
@@ -86,18 +56,38 @@ def show_dropdown(workout_type):
         swimming_frame.pack()
     elif workout_type == "Yoga":
         yoga_frame.pack()
-
-workout_var.trace("w", lambda *args: show_dropdown(workout_var.get()))
-
-# Create a list to display the workout history
-workout_list = tk.Listbox(root)
-workout_list.pack(pady=10)
-
-# Create buttons to add and remove workouts
-add_button = tk.Button(root, text="Add Workout", command=add_workout)
-add_button.pack(pady=10)
-remove_button = tk.Button(root, text="Remove Workout", command=remove_workout)
-remove_button.pack(pady=10)
-
-load_workouts()
-root.mainloop()
+# Triggered on workout type change
+def change_workout(*args):
+    try:
+        workout_type = workout_var.get()
+        workout_dropdown.config(image=workout_icons[workout_type])
+        show_dropdown(workout_type)
+    except KeyError as e:
+        print(f"The specified workout type is not an option: {e}")
+try:
+    workout_var.trace("w", change_workout)
+except Exception as e:
+    print(f"An unexpected error occurred when tracing the workout variable variable: {e}")
+try:
+    change_workout()
+except Exception as e:
+    print(f"An unexpected error occurred when changing the workout variable variable: {e}")
+# add workout button
+def add_workout():
+    workout_name = workout_var.get()
+    try:
+        sub_workout = locals()[workout_name.lower() + "_var"].get()
+        workout_list.insert("end", f"{workout_name}: {sub_workout}")
+    except AttributeError as e:
+        print(f"An attribute error has occurred: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred when inserting the list item: {e}")
+try:
+    add_workout_button = tk.Button(root, text="Add Workout", command=add_workout)
+    add_workout_button.pack(pady=10)
+except Exception as e:
+    print(f"An unexpected error occurred when creating the Add Workout button: {e}")
+try:
+    root.mainloop()
+except Exception as e:
+    print(f"An unexpected error occurred in mainloop: {e}")
