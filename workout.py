ea576eea7eae47ffcd9b09ab90ed956e99623c1a
Bullseyecowbelly <34012548+greengeckowizard@users.noreply.github.com>
2023-02-13 19:30:44 -0600

    we got somewhere lol

--- workout.py
+++ workout.py
@@ -1,93 +1,56 @@
 import tkinter as tk
-import os
-from tkinter import ttk
-from PIL import Image, ImageTk
+
 root = tk.Tk()
 root.title("Workout Log")
-# Workout list
-workout_list = tk.Listbox(root)
-workout_list.pack(pady=10)
-# Workout icons
-icon_path = "workout_icons"
-try:
-    workout_icons = {
-        "Running": ImageTk.PhotoImage(Image.open(os.path.join(icon_path, "running.png"))),
-        "Cycling": ImageTk.PhotoImage(Image.open(os.path.join(icon_path, "cycling.png"))),
-        "Weightlifting": ImageTk.PhotoImage(Image.open(os.path.join(icon_path, "weightlifting.png"))),
-        "Swimming": ImageTk.PhotoImage(Image.open(os.path.join(icon_path, "swimming.png"))),
-        "Yoga": ImageTk.PhotoImage(Image.open(os.path.join(icon_path, "yoga.png")))
-    }
-except FileNotFoundError as e:
-    print(f"The file could not be found: {e}")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
-# Workout dropdown
-workout_var = tk.StringVar(value="Running")
-workout_dropdown = tk.OptionMenu(root, workout_var, *workout_icons.keys(), image=workout_icons["Running"], compound="left")
-workout_dropdown.pack(pady=10)
-# Dropdown menu values
-workout_types = {
+
+# Create a dictionary of workout types and their corresponding exercises
+workouts = {
     "Weightlifting": ["Bench Press", "Deadlift", "Squat", "Bicep Curls", "Tricep Dips"],
-    "Swimming": ["Freestyle", "Backstroke", "Breaststroke", "Butterfly"],
+    "Cardio": ["Running", "Cycling", "Swimming"],
     "Yoga": ["Vinyasa", "Hatha", "Bikram", "Ashtanga", "Iyengar"]
 }
-# Subdropdown frames
-weightlifting_frame = tk.Frame(root)
-swimming_frame = tk.Frame(root)
-yoga_frame = tk.Frame(root)
-# Subdropdown vars and comboboxes
-weightlifting_var = tk.StringVar(value="Bench Press")
-weightlifting_dropdown = ttk.Combobox(weightlifting_frame, textvariable=weightlifting_var, values=workout_types["Weightlifting"])
-weightlifting_dropdown.pack(pady=10)
-swimming_var = tk.StringVar(value="Freestyle")
-swimming_dropdown = ttk.Combobox(swimming_frame, textvariable=swimming_var, values=workout_types["Swimming"])
-swimming_dropdown.pack(pady=10)
-yoga_var = tk.StringVar(value="Vinyasa")
-yoga_dropdown = ttk.Combobox(yoga_frame, textvariable=yoga_var, values=workout_types["Yoga"])
-yoga_dropdown.pack(pady=10)
-# Show relevant subdropdown
-def show_dropdown(workout_type):
-    weightlifting_frame.pack_forget()
-    swimming_frame.pack_forget()
-    yoga_frame.pack_forget()
-    if workout_type == "Weightlifting":
-        weightlifting_frame.pack()
-    elif workout_type == "Swimming":
-        swimming_frame.pack()
-    elif workout_type == "Yoga":
-        yoga_frame.pack()
-# Triggered on workout type change
-def change_workout(*args):
-    try:
-        workout_type = workout_var.get()
-        workout_dropdown.config(image=workout_icons[workout_type])
-        show_dropdown(workout_type)
-    except KeyError as e:
-        print(f"The specified workout type is not an option: {e}")
-try:
-    workout_var.trace("w", change_workout)
-except Exception as e:
-    print(f"An unexpected error occurred when tracing the workout variable variable: {e}")
-try:
-    change_workout()
-except Exception as e:
-    print(f"An unexpected error occurred when changing the workout variable variable: {e}")
-# add workout button
+
+# Create a drop-down menu for selecting the workout type
+workout_type_var = tk.StringVar(value="Weightlifting")
+workout_type_dropdown = tk.OptionMenu(root, workout_type_var, *workouts.keys())
+workout_type_dropdown.pack(pady=10)
+
+# Create a drop-down menu for selecting the specific exercise
+exercise_var = tk.StringVar(value=workouts[workout_type_var.get()][0])
+exercise_dropdown = tk.OptionMenu(root, exercise_var, *workouts[workout_type_var.get()])
+exercise_dropdown.pack(pady=10)
+
+# Update the exercise options when the workout type is changed
+def update_exercise_options(*args):
+    exercise_dropdown["menu"].delete(0, "end")
+    for exercise in workouts[workout_type_var.get()]:
+        exercise_dropdown["menu"].add_command(label=exercise, command=tk._setit(exercise_var, exercise))
+
+workout_type_var.trace("w", update_exercise_options)
+
+# Create an entry field for specifying the number of sets and reps
+sets_var = tk.StringVar(value="1")
+sets_label = tk.Label(root, text="Sets:")
+sets_label.pack(pady=10)
+sets_entry = tk.Entry(root, textvariable=sets_var)
+sets_entry.pack(pady=5)
+
+reps_var = tk.StringVar(value="10")
+reps_label = tk.Label(root, text="Reps:")
+reps_label.pack(pady=10)
+reps_entry = tk.Entry(root, textvariable=reps_var)
+reps_entry.pack(pady=5)
+
+# Create a list to display the workout history
+workout_list = tk.Listbox(root)
+workout_list.pack(pady=10)
+
+# Create a button to add the workout
 def add_workout():
-    workout_name = workout_var.get()
-    try:
-        sub_workout = locals()[workout_name.lower() + "_var"].get()
-        workout_list.insert("end", f"{workout_name}: {sub_workout}")
-    except AttributeError as e:
-        print(f"An attribute error has occurred: {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred when inserting the list item: {e}")
-try:
-    add_workout_button = tk.Button(root, text="Add Workout", command=add_workout)
-    add_workout_button.pack(pady=10)
-except Exception as e:
-    print(f"An unexpected error occurred when creating the Add Workout button: {e}")
-try:
-    root.mainloop()
-except Exception as e:
-    print(f"An unexpected error occurred in mainloop: {e}")
+    workout = f"{workout_type_var.get()} - {exercise_var.get()} - {sets_var.get()} sets, {reps_var.get()} reps"
+    workout_list.insert(tk.END, workout)
+
+add_button = tk.Button(root, text="Add Workout", command=add_workout)
+add_button.pack(pady=10)
+
+root.mainloop()
